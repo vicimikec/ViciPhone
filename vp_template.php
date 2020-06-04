d9c39bec82584629ae32806ffeec2742390bfe10
Christian Cabrera <ccabrerar@users.noreply.github.com>
2020-06-04 12:41:19 -0500

    Switch to minified CDN sip.js version

--- vp_template.php
+++ vp_template.php
@@ -133,7 +133,7 @@
 		<script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
 
 		<!-- SIP.js library, included from CDN. If you need it offline, uncomment the next line -->
-		<script src="https://cdn.jsdelivr.net/npm/sip.js@0.15.10/dist/sip-0.15.10.js"></script-->
+		<script src="https://cdn.jsdelivr.net/npm/sip.js@0.15.10/dist/sip-0.15.10.min.js"></script-->
 
 		<!-- SIP.js library offline version-->
 		<!--script src="js/sip-0.15.10.min.js"></script-->
